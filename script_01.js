cbef3eae2a7c8bf44e79d1fd35d3e182e1a108f7
Marinus <marinus.fehling@web.de>
2023-01-18 11:54:20 +0100

    Alle Rechenarten

--- script_01.js
+++ script_01.js
@@ -11,7 +11,41 @@
 4. Ausgabe in Konsole : check!
 */
 
+//Konstanten
+const ERROR_STR_DIV = "Man darf nicht durch Null teilen!!";
 
+// test
+output(div(2,2));
+output(div(2,-2));
+output(div(3,-2));
+output(div(2,0));
+
+
+function add(a, b)
+{
+	return a + b;
+}
+
+function sub(a, b)
+{
+	return a - b;
+}
+
+function mult(a, b)
+{
+	return a * b;
+}
+
+function div(a, b)
+{
+	if(b == 0)
+	{
+		return ERROR_STR_DIV;
+	}
+
+	return a / b;
+	
+}
 
 
 // module:  output | test:
